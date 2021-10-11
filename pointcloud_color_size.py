6a1379faf1a6d154553dd6f7d7e417b78248fb19
czirjakkethz <82337714+czirjakkethz@users.noreply.github.com>
2021-10-11 13:17:42 +0200

    Code refracture
    Code refracture + rough surface plotting (didn't add files last commit)

--- pointcloud_color_size.py
+++ pointcloud_color_size.py
@@ -6,184 +6,347 @@ from Bio.PDB import *
 from Bio import SeqIO
 import numpy as np
 import pyvista as pv
+import os
 
-# load structure form pdb file
-def load_structure(file_name):
+class StickPoint:
+    def __init__():
+        pass
 
-    # parse file and return structure
-    parser = PDBParser() 
-    load_pdb = file_name + ".pdb"
-    structure = parser.get_structure(file_name, load_pdb) 
-    return structure
+    def load_structure(file_name):
+        """
+        load structure form pdb file (by parsing file) and return structure
+        :param name: file_name - Name of file (without filetype) to be loaded
+        :param type: str
+        :return: structure - Biopython structure representation of the file
+        """
 
-# Creates a dictionary of atomic coordinates by atom type
+        parser = PDBParser() 
+        load_pdb = file_name + ".pdb"
+        structure = parser.get_structure(file_name, load_pdb) 
+        
+        return structure
 
-    # load file into a python list
-    # initialize dictionary, then fill up iteratively
-    atom_data = dict()
+    def get_atoms(structure, show_solvent=False):
+        """
+        Creates a dictionary of atomic coordinates from structure
+        :param name: structure - Biopython structure object of desired molecule
+        :param type: structure
+        :param name: show_solvent - If True solvent molecules also added to retrun dictionary
+        :param type: bool
+        :return: dict - Dictionary of atomic coordinates by atom type
+        """
+        # load file into a python list
+        residues = structure.get_residues()
+        residues_list = list(residues)
+        
+        # initialize dictionary, then fill up iteratively
+        atom_data = dict()
+        for residue in residues_list:
+            if residue.get_resname() == "HOH" and not show_solvent:
+                continue
+            for atom in residue:
+                type_name = atom.element
+                if type_name not in atom_data:
+                    atom_data[type_name] = [atom.get_coord()]
+                else:
+                    atom_data[type_name].append(atom.get_coord())
 
-    # return the 3D positions of atoms organized by atom type        
-
-
-# Creates a dictionary of coordinates by residues
-def get_residues(structure):
-
-    # load file into a python list
-    residues = structure.get_residues()
-    res_list = list(residues)
-
-    # initialize dictionary, then fill up iteratively
-    res_data = dict()
-    for res in res_list:
-        type_name = res.get_resname()
-        center_coord = [0., 0., 0.]
-        c = 0.
-        for atom in res:    
-            center_coord = center_coord + atom.get_coord()
-            c += 1.
-        center_coord = center_coord / c
-        if type_name not in res_data:
-            res_data[type_name] = [center_coord]
-        else:
-            res_data[type_name].append(center_coord)
-
-    # return the 3D positions of atoms organized by atom type        
-    return res_data
-
-
-# preprocessing for plotting by atom type
-def pre_plot_atoms(atom_data):
-
-    # Set resolution variables so only need to change once
-    phi_res = 10
-    theta_res = 10
-    # these two dictionaries have to be manually created
-    # also, if more/other atoms present in protein it will not work
-    atoms_size_dict = {'C': pv.Sphere(radius=0.67, phi_resolution=phi_res, theta_resolution=theta_res),
-                    'O': pv.Sphere(radius=0.48, phi_resolution=phi_res, theta_resolution=theta_res),
-                    'N': pv.Sphere(radius=0.56, phi_resolution=phi_res, theta_resolution=theta_res),
-                    'S': pv.Sphere(radius=0.88, phi_resolution=phi_res, theta_resolution=theta_res),
-                    'CA': pv.Sphere(radius=1.94, phi_resolution=phi_res, theta_resolution=theta_res)}
-    color_dict = {'C': '#333333',
-                    'O': '#EA2128',
-                    'N': '#007DEE',
-                    'S': '#FFDC61',
-                    'CA': '#38761D'}
-
-    # create glyphs (spherical) to represent each atom
-    atoms_spheres = []
-    colors_spheres = []
-    for atoms_type in atom_data:
-
-        # create a mesh with each atoms position
-        mesh = pv.PolyData(np.array(atom_data[atoms_type]))
-
-        # place a specific sphere at given position
-        glyphs = mesh.glyph(geom=atoms_size_dict[atoms_type])
-        atoms_spheres.append(glyphs)
-
-        #color the sphere according to 'CPK' standard
-        colors_spheres.append(color_dict[atoms_type])
-
-    # return list of spheres and colors representing each atom
-    return atoms_spheres, colors_spheres
+        # return the 3D positions of atoms organized by atom type        
+        return atom_data
 
 
-# preprocessing for plotting by atom type
-def pre_plot_residues(res_data):
+    def get_residues(structure):
+        """
+        Creates a dictionary of coordinates by residues from structure object
+        :param name: structure - Biopython structure object of desired molecule
+        :param type: structure
+        :return: dict - Dictionary of atomic coordinates by residue type
+        """
 
-    # Set resolution variables so only need to change once
-    phi_res = 25
-    theta_res = 25
-    # these two dictionaries have to be manually created
-    # could not find volume information for CYS, HIS, LYS, THR, TYR
-    # for above mentioned apporximation by "closest" available
-    res_size_dict = { "ALA": 2.80, "ARG": 3.77, "ASN": 3.18, "ASP": 1.51, 
-        "CYS": 2.87, "GLN": 3.37, "GLU": 1.68, "GLY": 2.51, "HIS": 3.65,
-        "ILE": 3.43, "LEU": 3.42, "LYS": 3.77, "MET": 3.44, "PHE": 3.65, 
-        "PRO": 3.13, "SER": 2.87, "THR": 2.87, "TRP": 3.54, "TYR": 3.65, 
-        "VAL": 3.24, "HOH":0}
+        # load file into a python list
+        residues = structure.get_residues()
+        res_list = list(residues)
+
+        # initialize dictionary, then fill up iteratively
+        res_data = dict()
+        for res in res_list:
+            type_name = res.get_resname()
+            center_coord = [0., 0., 0.]
+            c = 0.
+            for atom in res:    
+                center_coord = center_coord + atom.get_coord()
+                c += 1.
+            center_coord = center_coord / c
+            if type_name not in res_data:
+                res_data[type_name] = [center_coord]
+            else:
+                res_data[type_name].append(center_coord)
+
+        # return the 3D positions of atoms organized by atom type        
+        return res_data
 
 
-    color_dict = {"ALA": '#13B6E2', "ARG": '#23DEFE', "ASN": '#0EFF57', "ASP": '#822DD2', 
-        "CYS": '#A22282', "GLN": '#60C7B0', "GLU": '#6913FE', "GLY": '#8E41D0', "HIS": '#7089FC',
-        "ILE": '#0CAFF9', "LEU": '#806769', "LYS": '#8B7928', "MET": '#68D139', "PHE": '#8BA695', 
-        "PRO": '#9FEBA4', "SER": '#BBD7EB', "THR": '#D1A67A', "TRP": '#F93713', "TYR": '#E5613D', 
-        "VAL": '#128033', "HOH": 'w' }
+    def pre_plot_atoms(atom_data):
+        """
+        Create a list of Shperes representing each atom for plotting
+        :param name: atom_data - Dictionary of atoms and their coordinates, by atom type
+        :param type: dict
+        :return: list - List of pyvista Shperes representing each atom 
+        :return: list - List of colors for each atom
+        """
+        # Set resolution variables so only need to change once
+        phi_res = 10
+        theta_res = 10
+        # these two dictionaries have to be manually created
+        # also, if more/other atoms present in protein it will not work
+        atoms_size_dict = {'C': pv.Sphere(radius=0.67, phi_resolution=phi_res, theta_resolution=theta_res),
+                        'O': pv.Sphere(radius=0.48, phi_resolution=phi_res, theta_resolution=theta_res),
+                        'N': pv.Sphere(radius=0.56, phi_resolution=phi_res, theta_resolution=theta_res),
+                        'S': pv.Sphere(radius=0.88, phi_resolution=phi_res, theta_resolution=theta_res),
+                        'CA': pv.Sphere(radius=1.94, phi_resolution=phi_res, theta_resolution=theta_res)}
+        color_dict = {'C': '#333333',
+                        'O': '#EA2128',
+                        'N': '#007DEE',
+                        'S': '#FFDC61',
+                        'CA': '#38761D'}
 
-    # create glyphs (spherical) to represent each res
-    res_spheres = []
-    colors_spheres = []
-    for res_type in res_data:
+        # create glyphs (spherical) to represent each atom
+        atoms_spheres = []
+        colors_spheres = []
+        for atoms_type in atom_data:
 
-        shere = pv.Sphere(radius=res_size_dict[res_type], phi_resolution=phi_res, theta_resolution=theta_res)
+            # create a mesh with each atoms position
+            mesh = pv.PolyData(np.array(atom_data[atoms_type]))
 
-        # create a mesh with each residues position
-        mesh = pv.PolyData(np.array(res_data[res_type]))
+            # place a specific sphere at given position
+            glyphs = mesh.glyph(geom=atoms_size_dict[atoms_type])
+            atoms_spheres.append(glyphs)
 
-        # place a specific sphere at given position
-        glyphs = mesh.glyph(geom=shere)
-        res_spheres.append(glyphs)
+            #color the sphere according to 'CPK' standard
+            colors_spheres.append(color_dict[atoms_type])
 
-        #color the sphere according to 'CPK' standard
-        colors_spheres.append(color_dict[res_type])
+        # return list of spheres and colors representing each atom
+        return atoms_spheres, colors_spheres
 
-    # return list of spheres and colors representing each res
-    return res_spheres, colors_spheres
 
-def pre_plot_bonds(atom_list):
-    ns = NeighborSearch(atom_list)
+    def pre_plot_residues(res_data):
+        """
+        Create a list of Shperes representing each residue for plotting
+        :param name: res_data - Dictionary of residues and their coordinates, by residue type
+        :param type: dict
+        :return: list - List of pyvista Shperes representing each residue 
+        :return: list - List of colors for each residue
+        """
 
-    bonds = []
-    for target in atom_list:
-        close_atoms = ns.search(target.coord, _cutoff_dist)
-        my_coord = target.get_coord()
-        for close_atom in close_atoms:
-            # cylinder = pyvista.Cylinder(center=[1, 2, 3], direction=[1, 1, 1], 
-            #     radius=1, height=2)
-            c_coord = close_atom.get_coord()
-            line = pv.Line(my_coord, c_coord, resolution=5)
-            bonds.append(line)
-    
-    return bonds
+        # Set resolution variables so only need to change once
+        phi_res = 25
+        theta_res = 25
+        # these two dictionaries have to be manually created
+        # could not find volume information for CYS, HIS, LYS, THR, TYR
+        # for above mentioned apporximation by "closest" available
+        res_size_dict = { "ALA": 2.80, "ARG": 3.77, "ASN": 3.18, "ASP": 1.51, 
+            "CYS": 2.87, "GLN": 3.37, "GLU": 1.68, "GLY": 2.51, "HIS": 3.65,
+            "ILE": 3.43, "LEU": 3.42, "LYS": 3.77, "MET": 3.44, "PHE": 3.65, 
+            "PRO": 3.13, "SER": 2.87, "THR": 2.87, "TRP": 3.54, "TYR": 3.65, 
+            "VAL": 3.24, "HOH":0}
 
-# plot point cloud of atoms
-    # Use 3 lights because it's a bit brighter
+
+        color_dict = {"ALA": '#13B6E2', "ARG": '#23DEFE', "ASN": '#0EFF57', "ASP": '#822DD2', 
+            "CYS": '#A22282', "GLN": '#60C7B0', "GLU": '#6913FE', "GLY": '#8E41D0', "HIS": '#7089FC',
+            "ILE": '#0CAFF9', "LEU": '#806769', "LYS": '#8B7928', "MET": '#68D139', "PHE": '#8BA695', 
+            "PRO": '#9FEBA4', "SER": '#BBD7EB', "THR": '#D1A67A', "TRP": '#F93713', "TYR": '#E5613D', 
+            "VAL": '#128033', "HOH": 'w' }
+
+        # create glyphs (spherical) to represent each res
+        res_spheres = []
+        colors_spheres = []
+        for res_type in res_data:
+
+            shere = pv.Sphere(radius=res_size_dict[res_type], phi_resolution=phi_res, theta_resolution=theta_res)
+
+            # create a mesh with each residues position
+            mesh = pv.PolyData(np.array(res_data[res_type]))
+
+            # place a specific sphere at given position
+            glyphs = mesh.glyph(geom=shere)
+            res_spheres.append(glyphs)
+
+            #color the sphere according to 'CPK' standard
+            colors_spheres.append(color_dict[res_type])
+
+        # return list of spheres and colors representing each res
+        return res_spheres, colors_spheres
+
+    def pre_plot_bonds(struct):
+        """
+        Determine bonds from 3D information
+        :param name: struct - Biopython structure object of desired molecule
+        :param type: structure
+        :return: list - List of pyvista lines representing each bond 
+        """
+        # create a list of non-solvent atoms
+        residues = struct.get_residues()
+        residues_list = list(residues)
+        atom_list = []
+        for residue in residues_list:
+            if residue.get_resname() == "HOH":
+                continue
+            for atom in residue:
+                atom_list.append(atom)
+
+        ns = NeighborSearch(atom_list)
+        _cutoff_dist = 3 # not sure how to choose this number
+
+        bonds = []
+        for target in atom_list:
+            close_atoms = ns.search(target.coord, _cutoff_dist)
+            my_coord = target.get_coord()
+            for close_atom in close_atoms:
+                # cylinder = pyvista.Cylinder(center=[1, 2, 3], direction=[1, 1, 1], 
+                #     radius=1, height=2)
+                c_coord = close_atom.get_coord()
+                line = pv.Line(my_coord, c_coord, resolution=5)
+                bonds.append(line)
+        
+        return bonds
+
+    def plot_atoms(atoms, col_s, box=0, bonds=0, res=0, col_r=0):
+        """
+        Plot stick and point model
+        :param name: atoms - List of pyvista Shperes representing each atom 
+        :param type: list
+        :param name: col_s - List of colors for each atom
+        :param type: list
+        :param name: box - If True bounding box also visualized, default 0
+        :param type: bool        
+        :param name: bonds - List of pyvista Lines representing each bond, default 0 
+        :param type: list
+        :param name: res - List of pyvista Shperes representing each residue, default 0 
+        :param type: list
+        :param name: col_r - List of colors for each residue, default 0
+        :param type: list
+        :return: void - Window with interactive plot
+        """
+        # Use 3 lights because it's a bit brighter
+        pl = pv.Plotter(lighting=None)
+        pl.background_color = 'grey'
+        pl.enable_3_lights()
+
+        # adding the spheres (by atom type) one at a time
+        j = 0
+        for mesh in atoms:
+            pl.add_mesh(mesh, color=col_s[j])
+            j=j+1
+
+        # adding the bonds one at a time
+        if bonds != 0:
+            l = 0
+            for b in bonds:
+                pl.add_mesh(b, color='w', render_lines_as_tubes=True, line_width=5)
+        # adding the spheres (by residue) one at a time
+        # only executes if residue information provided
+        if res != 0:
+            k = 0
+            for mesh in res:
+                pl.add_mesh(mesh, color=col_r[k], opacity=0.2)
+                k+=1
+        
+        # if specified add bounding box
+        if box:
+            pl.add_bounding_box(color='white', corner_factor=0.5, line_width=1)
+
+        # save a screenshot
+        pl.show(screenshot='test.png')
+
+class Surface:
+    def __init__():
+        pass
+
+    def load(self, file_name):
+        """
+        Load surface information from both face and vert files
+        :param name: file_name - Name of input files
+        :param type: str
+        :return: list - list of face data
+        :return: list - list of vert data
+        """
+        
+        face = self.load_fv(file_name, ".face", 0)
+        vert = self.load_fv(file_name, ".vert", 1)
+        return face, vert
+
+
+    def load_fv(file_name, end, vorf):
+        """
+        Load surface information from face or vert file
+        :param name: file_name - Name of input file
+        :param type: str
+        :param name: end - Type of input file
+        :param type: str
+        :param name: vorf - Specify face or vertex
+        :param type: bool
+        :return: list - list of data
+        """
+        
+        outfile = open(file_name + end,"r")
+        data = outfile.readlines()
+        l3 = str.split(data[2])
+        numlines = int(l3[0])
+        numspheres = int(l3[1])
+        density = float(l3[2])
+        probe = float(l3[3])
+        ret = [[] for x in range(numlines)]
+        i = 0
+        for line in data[3:]:
+            line_split = str.split(line)
+            k = 0
+            for entry in line_split[:3]:
+                if vorf:
+                    ret[i].append(float(entry))
+                else:
+                    ret[i].append(int(entry))
+            i+=1
+
+        outfile.close()
+        return ret
+
+def surface_trying():
+    s = Surface
+    face, vertice = s.load(s, "2fd7_out")
+    vertices = np.array(vertice)
+    faces = np.hstack(face)
     pl = pv.Plotter(lighting=None)
     pl.background_color = 'grey'
     pl.enable_3_lights()
-
-    # adding the spheres (by atom type) one at a time
-    j = 0
-    for mesh in atoms_spheres:
-        pl.add_mesh(mesh, color=colors_spheres[j])
-        j=j+1
-
-    # adding the bonds one at a time
-    if bonds != 0:
-        l = 0
-        for b in bonds:
-            pl.add_mesh(b, color='w', render_lines_as_tubes=True, line_width=5)
-    # adding the spheres (by residue) one at a time
-    # only executes if residue information provided
-        k = 0
-        for mesh in r:
-            pl.add_mesh(mesh, color=c[j], opacity=0.2)
-            k+=1
+    # print(vertices)
+    tmesh = trimesh.Trimesh(vertice, faces=face, process=False)
+    mesh = pv.wrap(tmesh)
     
-    # if specified add bounding box
-    if box:
-        pl.add_bounding_box(color='white', corner_factor=0.5, line_width=1)
+    # geom = TriMeshGeom()
+    # geom.verts.resize(len(vertice))
+    # geom.faces.resize(len(face))
 
-    # save a screenshot
-    pl.show(screenshot='test.png')
-
+    # surf = pv.PolyData(vertice, face)
+    # surf.plot(scalars=np.arange(2707))
+    pl.add_mesh(mesh)
+    pl.show()
+    
+import trimesh
 def main():
-    
-    struct = load_structure("2fd7")
-    res_data = get_residues(struct)
-    atoms, colors = pre_plot_atoms(atom_data)
-    ress, colors_r = pre_plot_residues(res_data)
+    surface_trying()
+
+    # mesh = pv.Plane(i_resolution=3, j_resolution=3)
+    # mesh.verts = np.vstack((face,
+    #                         vert)).T
+    # mesh.plot(color='tan', render_points_as_spheres=True, point_size=60)
+    # sp = StickPoint
+    # struct = sp.load_structure("2fd7")
+    # atom_data = sp.get_atoms(struct) # second arg: 1 = showsolvent
+    # bonds = sp.pre_plot_bonds(struct)
+    # res_data = sp.get_residues(struct)
+    # atoms, colors = sp.pre_plot_atoms(atom_data)
+    # ress, colors_r = sp.pre_plot_residues(res_data)
+    # sp.plot_atoms(atoms, colors, 1, bonds, ress, colors_r)
 
 
 if __name__ == "__main__":
